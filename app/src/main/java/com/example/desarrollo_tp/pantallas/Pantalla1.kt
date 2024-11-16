e968bac4d61b2a749be8d3f9e0f50ad9476e611f
facku95 <facku1995@gmail.com>
2024-11-16 10:59:18 -0300

    primera pantalla

--- app/src/main/java/com/example/desarrollo_tp/pantallas/Pantalla1.kt
+++ app/src/main/java/com/example/desarrollo_tp/pantallas/Pantalla1.kt
@@ -8,11 +8,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.outlined.ExitToApp
@@ -20,6 +23,7 @@ import androidx.compose.material.icons.outlined.Menu
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
@@ -28,24 +32,26 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.desarrollo_tp.R
 import com.example.desarrollo_tp.R.drawable.unlam1
 import com.example.desarrollo_tp.componentes.header
+import com.example.desarrollo_tp.ui.theme.Gris
 
 import com.example.desarrollo_tp.ui.theme.Menta
+import com.example.desarrollo_tp.ui.theme.Negro1
 
 @Composable
 fun Pantalla1(){
 
-    Column(){
+    Column(modifier = Modifier.background(color = Gris)){
 
         header()
         body()
         footer()
-
-
     }
 
 }
@@ -54,6 +60,8 @@ fun Pantalla1(){
 @Composable
 fun body(){
     carrusel()
+    homecards()
+    carruseldepto()
 }
 
 @Composable
@@ -62,6 +70,7 @@ fun footer(){
 }
 @Composable
 fun carrusel(){
+
 
    Image(
        painter = painterResource(id = R.drawable.unlam4),
@@ -81,20 +90,115 @@ fun carrusel(){
                 Text("")
 
             }
-        OutlinedButton(onClick = {  }, modifier = Modifier.size(25.dp).padding(5.dp)) {
+        OutlinedButton(onClick = {  },
+            modifier = Modifier.size(25.dp).padding(5.dp)) {
             Text("")
         }
-        OutlinedButton(onClick = {  }, modifier = Modifier.size(25.dp).padding(5.dp)) {
+        OutlinedButton(onClick = {  },
+            modifier = Modifier.size(25.dp).padding(5.dp)) {
             Text("")
         }
-        OutlinedButton(onClick = {  }, modifier = Modifier.size(25.dp).padding(5.dp)) {
+        OutlinedButton(onClick = {  },
+            modifier = Modifier.size(25.dp).padding(5.dp)) {
             Text("")
         }
+        Spacer(modifier = Modifier.size(45.dp))
 
 
     }
 
 }
+
+@Composable
+fun homecards(){
+    Row (modifier = Modifier.fillMaxWidth().height(120.dp).background(color= Gris),
+        horizontalArrangement = Arrangement.Absolute.SpaceAround
+        ){
+
+        Card(modifier = Modifier.weight(1f).padding(7.dp)){
+            Text("Cursos",
+                modifier = Modifier.fillMaxSize().wrapContentSize(Alignment.Center),
+                
+                style = TextStyle(
+                    fontSize = 25.sp
+                ))
+
+        }
+        Card(modifier = Modifier.weight(1f).padding(7.dp)){
+            Text("Carreras",
+                modifier = Modifier.fillMaxSize().wrapContentSize(Alignment.Center),
+                style = TextStyle(
+                    fontSize = 25.sp
+                ))
+        }
+
+    }
+    Row (modifier = Modifier.fillMaxWidth().height(120.dp).background(color= Gris),
+        horizontalArrangement = Arrangement.Absolute.SpaceAround
+    ){
+
+        Card(modifier = Modifier.weight(1f).padding(7.dp)){
+            Text("Postgrado",
+                modifier = Modifier.fillMaxSize().wrapContentSize(Alignment.Center),
+                style = TextStyle(
+                    fontSize = 25.sp
+                ))
+        }
+        Card(modifier = Modifier.weight(1f).padding(7.dp)){
+            Text("Tecnicaturas",
+                modifier = Modifier.fillMaxSize().wrapContentSize(Alignment.Center),
+                style = TextStyle(
+                    fontSize = 25.sp
+                ))
+        }
+    }
+}
+@Composable
+fun carruseldepto(){
+    Row() {
+
+Card (modifier = Modifier.padding(10.dp)) {
+    Text(
+        "Departamentos",
+        modifier = Modifier.fillMaxWidth().wrapContentWidth(Alignment.CenterHorizontally).padding(3.dp),
+        style = TextStyle(
+            fontSize = 25.sp
+        )
+    )
+}
+
+
+    }
+    Spacer(modifier = Modifier.size(15.dp))
+    Row (
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.unlam4),
+            contentDescription = "imagen1",
+            modifier = Modifier.weight(1f).padding(7.dp)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.unlam4),
+            contentDescription = "imagen1",
+            modifier = Modifier.weight(1f).padding(7.dp)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.unlam4),
+            contentDescription = "imagen1",
+            modifier = Modifier.weight(1f).padding(7.dp)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.unlam4),
+            contentDescription = "imagen1",
+            modifier = Modifier.weight(1f).padding(7.dp)
+        )
+    }
+}
+
+
 
 @Preview(showSystemUi = true)
 @Composable
--- app/src/main/java/com/example/desarrollo_tp/ui/theme/Color.kt
+++ app/src/main/java/com/example/desarrollo_tp/ui/theme/Color.kt
@@ -6,6 +6,9 @@ val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 val Menta = Color(0xFF168C6B)
+val Gris = Color(0xFF262626)
+val Negro1 = Color(0xFF121212)
+
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
