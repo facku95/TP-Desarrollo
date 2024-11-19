e17c6c85ff4329df0f3ae623153c1ff46cc0b950
FranFox100 <fyanezpioch292@alumno.unlam.edu.ar>
2024-11-18 23:12:41 -0300

    Funcionalidad de los botones
    TO DO: PantallaAgenda

--- app/src/main/java/com/example/desarrollo_tp/pantallas/Pantalla2.kt
+++ app/src/main/java/com/example/desarrollo_tp/pantallas/Pantalla2.kt
@@ -13,6 +13,9 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -26,39 +29,53 @@ import com.example.desarrollo_tp.componentes.Calendario
 import com.example.desarrollo_tp.ui.theme.Gris
 import com.example.desarrollo_tp.ui.theme.Menta
 
+
 @Composable
 fun Pantalla2(modifier: Modifier = Modifier.fillMaxSize(),
               controller: NavHostController
-){
+) {
 
     Column(modifier = Modifier.fillMaxSize().background(color = Gris)) {
+        var pantallaCalendario = remember { mutableStateOf(true) }
+        var pantallaAgenda = remember { mutableStateOf(false) }
+        Spacer(modifier = Modifier.size(80.dp))
 
-    Spacer(modifier = Modifier.size(50.dp))
+        Row(modifier = Modifier.fillMaxWidth()) {
 
-        Row (modifier = Modifier.fillMaxWidth()) {
+            Spacer(modifier = Modifier.size(30.dp))
 
-    Spacer(modifier = Modifier.size(30.dp))
-
-            Button(modifier = Modifier.size(150.dp, 80.dp),
+            Button(modifier = Modifier.size(150.dp, 60.dp),
                 colors = ButtonDefaults.buttonColors(containerColor = Menta),
-                onClick = {}) {
-                Text(text =  "Calendario",
+                onClick = {
+
+                    pantallaAgenda.value = false
+                    pantallaCalendario.value = true
+
+                }) {
+                Text(
+                    text = "Calendario",
                     modifier = Modifier.fillMaxWidth(),
                     textAlign = TextAlign.Center,
                     fontSize = 18.sp
-                    )
+                )
             }
 
             Spacer(modifier = Modifier.size(30.dp))
 
-            Button(modifier = Modifier.size(150.dp, 80.dp),
+            Button(modifier = Modifier.size(150.dp, 60.dp),
                 colors = ButtonDefaults.buttonColors(containerColor = Menta),
-                onClick = {}) {
-                Text(text = "Agenda",
+                onClick = {
+
+                    pantallaCalendario.value = false
+                    pantallaAgenda.value = true
+
+                }) {
+                Text(
+                    text = "Agenda",
                     modifier = Modifier.fillMaxWidth(),
                     textAlign = TextAlign.Center,
                     fontSize = 18.sp
-                    )
+                )
             }
 
             Spacer(modifier = Modifier.size(30.dp))
@@ -67,9 +84,23 @@ fun Pantalla2(modifier: Modifier = Modifier.fillMaxSize(),
 
         Spacer(modifier = Modifier.size(40.dp))
 
-        Calendario(modifier = Modifier.fillMaxHeight())
-
+        if (pantallaAgenda.value) {
+            PantallaAgenda()
+        }
+        if (pantallaCalendario.value) {
+            PantallaCalendario()
+        }
 
     }
+
+}
+
+@Composable
+fun PantallaCalendario(){
+    Calendario(Modifier.fillMaxHeight())
+}
+
+@Composable
+fun PantallaAgenda() {
 
 }
